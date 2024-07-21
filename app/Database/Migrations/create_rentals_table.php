bdf4135fe7d0a436f2ad94c86cbcead241b0bf85
meyrini <meyrini202@std.unissula.ac.id>
2024-07-22 00:45:42 +0700

    create_rentals_table.php

---
+++ app/Database/Migrations/create_rentals_table.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateRentalsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'price' => [
+                'type' => 'DECIMAL',
+                'constraint' => '10,2',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('rentals');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('rentals');
+    }
+}
