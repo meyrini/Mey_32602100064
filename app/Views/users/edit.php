01c91725705ea56a924eff6bcd4a44ab237235a6
meyrini <meyrini202@std.unissula.ac.id>
2024-07-22 04:34:54 +0700

    Menambahkan edit.php

--- app/Views/users/edit.php
+++ app/Views/users/edit.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Edit Pengguna</title>
-    <link rel="stylesheet" type="text/css" href="<?= base_url('css/style.css') ?>">
+    <title>Edit User</title>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-    <h1>Edit Pengguna</h1>
+    <h1>Edit User</h1>
     <form action="/users/update/<?= $user['id'] ?>" method="post">
-        <label for="name">Nama:</label>
+        <label>Name</label>
         <input type="text" name="name" value="<?= $user['name'] ?>" required><br>
-        <label for="email">Email:</label>
+        <label>Email</label>
         <input type="email" name="email" value="<?= $user['email'] ?>" required><br>
-        <label for="password">Password:</label>
+        <label>Password</label>
         <input type="password" name="password" required><br>
-        <input type="submit" value="Update" class="btn">
+        <button type="submit">Update</button>
     </form>
 </body>
 </html>
