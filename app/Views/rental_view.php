2c75636f5c097b57caf43636e11c5d357c8c6cdb
meyrini <meyrini202@std.unissula.ac.id>
2024-07-22 00:58:41 +0700

    rental_view.php

--- app/Views/rental_view.php
+++ app/Views/rental_view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rental List</title>
-    <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/rental.css') ?>">
 </head>
 <body>
     <header>
