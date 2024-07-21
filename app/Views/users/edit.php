02055d1c5de2caa9e27b89ba459c1309044f4e8e
meyrini <meyrini202@std.unissula.ac.id>
2024-07-22 04:54:56 +0700

    Menambahkan edit.php

--- app/Views/users/edit.php
+++ app/Views/users/edit.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Edit User</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="public/assets/css/style.css">
 </head>
 <body>
     <h1>Edit User</h1>
