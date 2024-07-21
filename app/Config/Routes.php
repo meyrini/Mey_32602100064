7cc3a233715ec3b10f1bd2433954fe336ee3ef1f
meyrini <meyrini202@std.unissula.ac.id>
2024-07-22 01:00:20 +0700

    Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,27 +2,22 @@
 
 namespace Config;
 
+// Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
-if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
-    require SYSTEMPATH . 'Config/Routes.php';
+// Load the system's routing file first, so that the app and ENVIRONMENT
+// can override as needed.
+if (file_exists(APPPATH . 'Config/Routes.php')) {
+    require APPPATH . 'Config/Routes.php';
 }
 
-$routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
-$routes->setDefaultMethod('index');
-$routes->setTranslateURIDashes(false);
-$routes->set404Override();
-$routes->setAutoRoute(true);
+// Define routes for the Rental controller
+$routes->get('rental', 'Rental::index');
+$routes->get('rental/create', 'Rental::create');
+$routes->post('rental/store', 'Rental::store');
+$routes->get('rental/edit/(:num)', 'Rental::edit/$1');
+$routes->post('rental/update', 'Rental::update');
+$routes->get('rental/delete/(:num)', 'Rental::delete/$1');
 
-$routes->get('/', 'Home::index');
-$routes->get('/users', 'UserController::index');
-$routes->get('/users/create', 'UserController::create');
-$routes->post('/users/store', 'UserController::store');
-$routes->get('/users/edit/(:num)', 'UserController::edit/$1');
-$routes->post('/users/update/(:num)', 'UserController::update/$1');
-$routes->get('/users/delete/(:num)', 'UserController::delete/$1');
-
-if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
-    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
-}
+// The default route (optional, depending on your application needs)
+$routes->get('/', 'Rental::index');
